4779c9d14aa88918fa60f5a1b2856aafca5de0d1
ahabhgk <hi@ahabhgk.top>
2019-09-15 17:13:32 +0800

    hooks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react'
 import Todo from './Todo.jsx'
 import { TodosContext } from '../stores/Todos.jsx'
+import { VisibileFilterContext } from '../stores/VisibileFilter.jsx'
 import { toggleTodo } from '../actions/index.js'
 
 const TodoList = () => {
   const { state: todos, dispatch } = useContext(TodosContext)
+  const { state: filter } = useContext(VisibileFilterContext)
+
+  const visibileTodos = todos.filter(todo => {
+    if (filter === 'SHOW_COMPLETED') {
+      return todo.completed
+    }
+    if (filter === 'SHOW_ACTIVE') {
+      return !todo.completed
+    }
+    return todo
+  })
 
   return (
     <ul>
-      {todos.map((todo) => (
+      {visibileTodos.map((todo) => (
         <Todo key={todo.id} {...todo} onClick={() => dispatch(toggleTodo(todo.id))} />
       ))}
     </ul>
